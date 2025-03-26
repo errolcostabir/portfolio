63e84045add5eac3d1d0e0433cffbd99488a30f3
errolcostabir <errolcostabir98@gmail.com>
2025-03-26 23:06:46 +0530

    fifth commit

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
           <li className="tracking-[-.01em]">
-            Welcome Vercel
+            Welcome to Vercel
           </li>
         </ol>
 
